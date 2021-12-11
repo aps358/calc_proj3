dfa429235ad6451dfa7473bcd7e6f6297ce18741
aps358 <as358@njit.edu>
2021-12-10 22:38:39 -0500

    Beautify Tables Files Updated

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 """A simple flask web app"""
-from flask import Flask, request
+from flask import Flask, request, flash
 from flask import render_template
 from calculator.main import Calculator
 app = Flask(__name__)
@@ -22,23 +22,24 @@ def basicform():
         #this will call the correct operation
         getattr(Calculator, operation)(my_tuple)
         result = str(Calculator.get_last_result_value())
-        return render_template('result.html',value1=value1, value2=value2, operation=operation, result=result)
+        return render_template('result.html', value1=value1, value2=value2, operation=operation, result=result)
     # Displays the form because if it isn't a post it is a get request
     else:
         return render_template('basicform.html')
 
 
 @app.route("/bad/<value1>/<value2>")
-def bad_calc(value1,value2):
+def bad_calc(value1, value2):
     """bad calc Route Response"""
     result = value1 + value2
     response = "The result of the calculation is: " + result + '<a href="/"> back</a>'
     return response
 
+
 @app.route("/good/<float:value1>/<float:value2>")
-def good_calc(value1,value2):
+def good_calc(value1, value2):
     """good calc Route Response"""
-    my_tuple = (value1,value2)
+    my_tuple = (value1, value2)
     Calculator.addition(my_tuple)
     response = "The result of the calculation is: " + str(Calculator.get_last_result_value()) + '<a href="/"> back</a>'
     return response
